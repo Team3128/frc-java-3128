5e702c72b74d8c23f2880da5eea818912f4bc5f2
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2014-12-14 19:41:06 -0800

    Initial tachometer interface code

---
+++ src/org/team3128/hardware/misc/TachLink.java
@@ -0,0 +1,83 @@
+package org.team3128.hardware.misc;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.team3128.Options;
+import edu.wpi.first.wpilibj.I2C;
+
+public class TachLink
+{
+	private static I2C _tachConnection;
+	
+	// lock this mutex whenever you want to use _tachConnection
+	private static ReentrantLock _tachConnMutex;
+	
+	private int _tachNumber;
+	
+	private int _toothNumber;
+	
+	final static int MEASURMENTS_PER_SECOND = 1;
+	
+	public TachLink(int tachNumber, int toothNumber)
+	{
+		if(tachNumber >= 8)
+		{
+			throw new IllegalArgumentException("tachometer number must be between 1 and 7");
+		}
+		
+		_tachNumber = tachNumber;
+		
+		//init static variables if this is the first use of the class
+		if(_tachConnection == null)
+		{
+			_tachConnection = new I2C(Options.instance()._tachI2CAddress);
+			
+			_tachConnMutex = new ReentrantLock();
+		}
+		
+		_toothNumber = toothNumber;
+	}
+	
+	/**
+	 * From the tachometer, get the number of teeth it's seen in the last measurement period
+	 * 
+	 * Time-wise, this is a relatively expensive operation, so don't call this function unnecessarily.
+	 */
+	public int getRaw()
+	{
+		_tachConnMutex.lock();
+		
+		//write tach number
+		_tachConnection.transaction(new byte[]{(byte) _tachNumber}, 1, new byte[0], 0);
+		
+		//read raw value
+		byte[] result = new byte[4];
+		_tachConnection.transaction(new byte[0], 0, result, 4);
+		int rawValue = 0;
+		
+		for(int counter = 0; counter < 4; ++counter)
+		{
+			rawValue |= result[3 - counter] << counter;
+		}
+		
+		_tachConnMutex.unlock();
+		
+		return rawValue;
+	}
+	
+	/**
+	 * From the tachometer, get the RPM of the gear.
+	 * 
+	 * Time-wise, this is a relatively expensive operation, so don't call this function unnecessarily.
+	 */
+	public int getRPM()
+	{
+		int rawValue = getRaw();
+		
+		return (60 * MEASURMENTS_PER_SECOND) * (rawValue / _toothNumber);
+	}
+	
+	
+	
+	
+}
