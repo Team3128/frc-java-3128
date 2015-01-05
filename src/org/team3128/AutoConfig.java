001294e00c6412d2c5341a42086250b82b2469f0
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-01-05 07:05:53 -0800

    add kit bot test code

--- src/org/team3128/AutoConfig.java
+++ src/org/team3128/AutoConfig.java
@@ -6,10 +6,24 @@ public class AutoConfig
 {
 	public static void initialize(Global global)
 	{
-		@SuppressWarnings("unused")
-		Instant sequenceStartTime = Instant.now();
-	
-		Log.info("AutoConfig", "Starting Autonomous Sequence...");
+		try
+		{
+			@SuppressWarnings("unused")
+			Instant sequenceStartTime = Instant.now();
+		
+			Log.info("AutoConfig", "Starting Autonomous Sequence...");
+			
+			global._motorLeftFront.setSpeed(.5);
+			Thread.sleep(500);
+			global._motorLeftBack.setSpeed(.5);
+			Thread.sleep(500);
+			global._motorRightFront.setSpeed(.5);
+			Thread.sleep(500);
+			global._motorRightBack.setSpeed(.5);
+		} catch (InterruptedException e)
+		{
+			return;
+		}
 	}
 
 }
--- src/org/team3128/Global.java
+++ src/org/team3128/Global.java
@@ -1,9 +1,11 @@
 package org.team3128;
 
 import org.team3128.hardware.misc.TachLink;
+import org.team3128.hardware.motor.MotorLink;
 import org.team3128.listener.ListenerManager;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * The Global class is where all of the hardware objects that represent the robot are stored.
@@ -17,9 +19,23 @@ public class Global
 {
 	public ListenerManager _listenerManager;
 	
+	public MotorLink _motorLeftFront;
+
+	public MotorLink _motorLeftBack;
+
+	public MotorLink _motorRightFront;
+
+	public MotorLink _motorRightBack;
+
+	
 	public Global()
 	{	
 		_listenerManager = new ListenerManager(new Joystick(Options.instance()._controllerPort));
+		
+		_motorLeftFront = new MotorLink(new Talon(0));
+		_motorLeftBack = new MotorLink(new Talon(1));
+		_motorRightFront = new MotorLink(new Talon(2));
+		_motorRightBack = new MotorLink(new Talon(3));
 
 	}
 
