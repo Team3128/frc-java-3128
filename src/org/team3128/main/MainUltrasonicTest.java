9aec646e6ed53f15f77d979154575c9a8fa3211f
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-09-05 21:35:14 -0700

    Add Ultrasonic Test main class

---
+++ src/org/team3128/main/MainUltrasonicTest.java
@@ -0,0 +1,60 @@
+package org.team3128.main;
+
+import org.team3128.MainClass;
+import org.team3128.RobotTemplate;
+import org.team3128.hardware.ultrasonic.IUltrasonic;
+import org.team3128.hardware.ultrasonic.MaxSonar;
+import org.team3128.listener.ListenerManager;
+import org.team3128.listener.controller.ControllerXbox;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+public class MainUltrasonicTest extends MainClass {
+
+	public ListenerManager lm;
+
+	public IUltrasonic testUltrasonic;
+	
+	public MainUltrasonicTest() 
+	{
+		lm = new ListenerManager(new Joystick(0), ControllerXbox.instance);
+
+		testUltrasonic = new MaxSonar(0);
+	}
+
+	@Override
+	protected void initializeRobot(RobotTemplate robotTemplate) 
+	{
+		robotTemplate.addListenerManager(lm);
+	}
+
+	@Override
+	protected void addAutoPrograms(SendableChooser autoChooser) {
+
+	}
+
+	@Override
+	protected void initializeDisabled() {
+
+	}
+
+	@Override
+	protected void updateDashboard() {
+
+	}
+
+	@Override
+	protected void initializeAuto() {
+
+	}
+
+	@Override
+	protected void initializeTeleop()
+	{
+		lm.addListener(ControllerXbox.XDOWN, () -> System.out.println(testUltrasonic.getDistance()));
+		lm.addListener(ControllerXbox.TRIGGERL, () -> testUltrasonic.setAutoPing(false));
+		lm.addListener(ControllerXbox.TRIGGERR, () -> testUltrasonic.setAutoPing(true));
+	}
+
+}
