4d7ed2737b0477e150651fd5e1af9cf62f18673f
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-09-09 07:40:08 -0700

    Start changing MaxSonar constructor.

--- src/org/team3128/hardware/ultrasonic/MaxSonar.java
+++ src/org/team3128/hardware/ultrasonic/MaxSonar.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.SerialPort.StopBits;
 
 /**
- * Class to read the Maxbotics MaxSonar MB1013 sensor plugged into the RoboRIO RS232 port.
+ * Class to read the Maxbotics MaxSonar sensors plugged into the RoboRIO RS232 port.
  * It is accurate to a few millimeters and has a max range of 500 cm.
  * 
  * The distance is updated asynchronously every half-second in auto-ping mode.
@@ -32,18 +32,21 @@ public class MaxSonar extends IUltrasonic
 	
 	AtomicInteger distanceMM;
 	
+	double units;
 	
 	boolean autoPing = true;
 	
 	/**
 	 * 
-	 * @param rangingPinDIONumber
+	 * @param rangingPinDIONumber The DIO pin that the ranging pin on the ultrasonic is connected to.
+	 * @param resolution The resolution from the Units class of the sensor as described by the MaxBotix website.  The MB1013 is mm, and the MB1010 is inch.
+	 * @param portToUse which serial port to use.
 	 */
-	public MaxSonar(int rangingPinDIONumber)
+	public MaxSonar(int rangingPinDIONumber, double resolution, Port portToUse)
 	{
 		distanceMM = new AtomicInteger();
 		
-		ultrasonicPort = new SerialPort(9600, Port.kOnboard, 8, Parity.kNone, StopBits.kOne);
+		ultrasonicPort = new SerialPort(9600, portToUse, 8, Parity.kNone, StopBits.kOne);
 		ultrasonicPort.enableTermination('\r');
 		ultrasonicPort.setTimeout(2);
 		
